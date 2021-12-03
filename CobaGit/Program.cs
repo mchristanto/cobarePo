026ce55e91a212720dc8fcee054e2d7a03ddd7ca
mchristanto <mchristanto01@student.ciputra.ac.id>
2021-12-03 09:24:27 +0700

    Initial check-in of module CobaGit

---
+++ CobaGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CobaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
